e006ecf2fa31e86857270518b839e0f4948ef255
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-26 23:14:03 +0700

    Update config-admin.js

--- config-admin.js
+++ config-admin.js
@@ -6,5 +6,5 @@ const CONFIG = {
      * ต้องเป็น URL ที่ได้จากการ Deploy สคริปต์ของ Admin โดยเฉพาะ
      * ที่มีฟังก์ชันสำหรับ Admin เช่น getUsers, getAllHistory, updateUserAdmin ฯลฯ
      */
-    GAS_URL_ADMIN: 'https://script.google.com/macros/s/AKfycbw7dfgZMuR-le-2TSiQ1hf90Q4W2thJq55FWeFVKjfpJE9e4Jo3Zt91dAIA2HSfqP-yEw/exec' // <--- !!! วาง URL ที่ได้จากการ Deploy สคริปต์ของ Admin ที่นี่ !!!
+    GAS_URL_ADMIN: 'https://script.google.com/macros/s/AKfycby5QApM9vHn2A_9nCgPgJdzu4p7PATJokPOHkBDVWyXLqP2pE6bAWwyom_OdYkDqpm3/exec' // <--- !!! วาง URL ที่ได้จากการ Deploy สคริปต์ของ Admin ที่นี่ !!!
 };
