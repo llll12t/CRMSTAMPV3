751f3ab7d0d78d7dda391ce87e1207d45f1097d8
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-26 22:54:20 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzn2FEor1rOwDg1msDe-RbiLl0TcuZhnGMIsyOpa38k2QY4Nr3MiukIMUHnuBWfrYqoaQ/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxEVn4eN8gQwYnfoAFOoMGnN60v5LwnuZBOApny5X8msERg7Mij-6CN7cMa77QX0_Pd/exec',
   LIFF_ID: '2007514355-PRvqxKwk',
   LIFF_REGISTER_ID:'2007514355-p1BnW75v'
 };
